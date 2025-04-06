3d5bb73ac801cf89af628ed0b621730eddcb4020
Ruben Rebelo <rubenrrebelo@users.noreply.github.com>
2025-04-06 22:57:15 +0100

    Update UI

---
+++ Assets/Scripts/RestartGame.cs
@@ -0,0 +1,11 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartGame : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
