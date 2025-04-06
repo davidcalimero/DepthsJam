9ed196f31367759dff599869bcbef42a11462af9
David Silva <herytic@gmail.com>
2025-04-06 17:35:07 +0200

    score

--- Assets/Scripts/BuildingNode.cs
+++ Assets/Scripts/BuildingNode.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
-using TMPro;
+﻿using TMPro;
 
 public class BuildingNode : BlockNode
 {
     public TextMeshPro text;
     public int targetAmount = 0;
     public int currentAmount = 0;
+    public int points = 0;
+
+    private bool objectiveReached = false;
 
     void OnGUI()
     {
+        if(!objectiveReached && currentAmount >= targetAmount)
+        {
+            objectiveReached = true;
+            GameManager.Instance.AddPoints(points * currentAmount);
+        }
         text.text = currentAmount + "/" + targetAmount;
     }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private int score = 0;
+
+    [Header("UI")]
+    public TMP_Text scoreText;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    public void AddPoints(int amount)
+    {
+        score += amount;
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        if (scoreText != null)
+            scoreText.text = $"Score: {score}";
+    }
+}
