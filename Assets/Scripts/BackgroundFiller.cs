d316295dc0040fb5ed732eb07cf1f6a89354970d
David Silva <herytic@gmail.com>
2025-04-06 16:55:56 +0200

    background filler

--- Assets/Scripts/BackgroundFiller.cs
+++ Assets/Scripts/BackgroundFiller.cs
@@ -1,15 +1,56 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class BackgroundFiller : MonoBehaviour
 {
-    public GameObject[] backgroundPrefabs;
+    public GameObject backgroundTilePrefab;
+    public float tileHeight;
 
-    private void Start()
+    private List<GameObject> spawnedTiles = new();
+    private Camera cam;
+
+    void Start()
     {
-        GenerateBackground(-1, -1, 1, 1, 0.05f, 0.01f);
+        cam = Camera.main;
+
+        GameObject first = Instantiate(backgroundTilePrefab, Vector3.zero, Quaternion.identity, transform);
+        spawnedTiles.Add(first);
+        tileHeight = CalculateBackgroundHeight(first);
     }
-    public void GenerateBackground(float minX, float maxX, float minY, float maxY, float piecesScale, float distanceBetween)
-    {
 
+    void Update()
+    {
+        CheckBackground();
+    }
+
+    void CheckBackground()
+    {
+        GameObject lastTile = spawnedTiles[spawnedTiles.Count - 1];
+        float cameraBottom = cam.transform.position.y - cam.orthographicSize;
+        float lastTileBottom = lastTile.transform.position.y - tileHeight / 2f;
+
+        if (cameraBottom < lastTileBottom + tileHeight)
+        {
+            Vector3 newPos = lastTile.transform.position - new Vector3(0, tileHeight, 0);
+            newPos.y += 3;
+            GameObject newTile = Instantiate(backgroundTilePrefab, newPos, Quaternion.identity, transform);
+            spawnedTiles.Add(newTile);
+        }
+    }
+
+    float CalculateBackgroundHeight(GameObject prefab)
+    {
+        SpriteRenderer[] renderers = prefab.GetComponentsInChildren<SpriteRenderer>(true);
+
+        if (renderers.Length == 0)
+            return 0;
+
+        Bounds bounds = renderers[0].bounds;
+        foreach (var r in renderers)
+        {
+            bounds.Encapsulate(r.bounds);
+        }
+
+        return bounds.size.y;
     }
 }
